07542f2f36fc5e36c80efe1f1dd70efd55b2dad5
Sarrah Gandhi <thesarrahgandhi@gmail.com>
2024-11-25 12:52:51 -0500

    final changes

--- HTML-Assignment3-SarrahGandhi/script.js
+++ HTML-Assignment3-SarrahGandhi/script.js
@@ -1,30 +1,32 @@
-// for tablet and phone toggle menu
 function menuClick() {
   console.log("Clicked menu");
+  // const element = document.getElementById("menu");
+  // element.classList.toggle("underline");
+
   const menuOption = document.getElementById("menuOption");
   menuOption.classList.toggle("hidden");
 }
 window.onload = validateForm;
-// For validating form and onsubmit display
+
 function validateForm() {
   const formhandle = document.forms["contact-form"];
-  const message = document.getElementById("result");
+  const message = document.getElementById("result"); // Corrected this line
   formhandle.onsubmit = processForm;
 
   function processForm() {
     if (formhandle.name.value === "") {
       formhandle.name.focus();
-      return false;
+      return false; // Prevent the form submission
     }
     if (formhandle.email.value === "") {
       formhandle.email.focus();
-      return false;
+      return false; // Prevent the form submission
     }
     if (formhandle.message.value === "") {
       formhandle.message.focus();
-      return false;
+      return false; // Prevent the form submission
     }
     message.style.display = "block";
-    return false;
+    return false; // Prevent the form submission
   }
 }
